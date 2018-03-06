278e923ecc48d9fa32acf43026827c395b9cb183
JackRMartin <36291367+JackRMartin@users.noreply.github.com>
2018-03-06 15:05:32 -0800

    Update FizzBuzz.cpp

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -1,1 +1,31 @@
+// FizzBuzz in C++
+// Jack Martin
+// This program will output numbers 1-100, but if the number is divisible by 3, output Fizz. If number is divisible by 5 output Buzz
+// If the number is divisible by 3 and 5, output FizzBuzz.
 
+#include <iostream>	
+
+int main()
+{
+
+	for (int i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0)
+		{
+			std::cout << "Fizz";
+		}
+		if(i % 5 == 0)
+		{
+			std::cout << "Buzz";
+		}
+		if (!(i % 3 == 0 || i % 5 == 0))
+		{
+			std::cout << i;
+		}
+		std::cout << std::endl;
+	}
+
+	
+	system("pause");
+	return 0;
+}
